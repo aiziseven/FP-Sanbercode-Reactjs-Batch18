e14657c4cb07710e983a8148e380d5b48f361b9b
aiziseven <zuliardiirfan@gmail.com>
2020-10-25 10:19:13 +0700

    update di hari minggu lagi

--- src/components/Menu/Header/Header.js
+++ src/components/Menu/Header/Header.js
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react';
 import { Layout, Menu } from 'antd';
-import { LoginOutlined, LogoutOutlined } from '@ant-design/icons';
+import { LoginOutlined, LogoutOutlined, UserOutlined } from '@ant-design/icons';
 import { Link } from 'react-router-dom';
 import { AppContext } from '../../../context/AppContext';
 
@@ -21,9 +21,14 @@ const HeaderMenu = () => {
             <Menu style={{ float: 'right' }} theme="dark" mode="horizontal" selectedKeys={0}>
                 {
                     login === 1 || localStorage.getItem('isLogin') && localStorage.getItem('user') ?
-                        <Menu.Item key="7" icon={<LogoutOutlined />} onClick={handleOut}>
-                            <Link to='/' >Logout</Link>
-                        </Menu.Item>
+                        <>
+                            <Menu.Item key="20" icon={<UserOutlined />}>
+                                Hello {JSON.parse(localStorage.getItem('user')).name}
+                            </Menu.Item>
+                            <Menu.Item key="7" icon={<LogoutOutlined />} onClick={handleOut}>
+                                <Link to='/' >Logout</Link>
+                            </Menu.Item>
+                        </>
                         :
                         <Menu.Item key="8" icon={<LoginOutlined />}>
                             <Link to='/' >Login</Link>
